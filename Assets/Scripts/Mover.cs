bfa996897e9e27c7c389de12eb45b1dbfd77d6ac
matthew holdsworth <mattholdsworth90@gmail.com>
2018-01-24 22:54:11 +1000

    Initial Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+	}
+	
+}
